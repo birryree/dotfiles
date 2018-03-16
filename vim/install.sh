3675003a0025251a0ccf9e00d99f7f93aae5f0a2
William Lee <wkl@fb.com>
2018-03-16 01:25:26 -0400

    Adding vim-airline and vim colorschemes

--- vim/install.sh
+++ vim/install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 mkdir -p ${HOME}/.vim/bundle
+mkdir -p ${HOME}/.vim/colors
 git clone git@github.com:VundleVim/Vundle.vim.git  ${HOME}/.vim/bundle/Vundle.vim
+cp $(dirname $0)/colors/* ${HOME}/.vim/colors/
 vim +PluginInstall +qall
